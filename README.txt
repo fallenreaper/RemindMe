c9c56b7fa99f0580ce3c9c93cb4a2a95e9224620
Fallenreaper <wllm.francis@gmail.com>
2018-07-18 22:27:55 -0400

    some base files

---
+++ README.txt
@@ -0,0 +1,7 @@
+=======
+RemindMe Bot
+=======
+
+Utility to monitor and respond to users in a timely fashion in regards to reminding them of a tweet they want to revisit at a defined later date.
+
+
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name="RemindMe",
+	version="0.1dev"
+	author="William Francis",
+	author_email="wllm.francis@gmail.com"
+	packages=["remindme"]
+	license="LICENSE"
+	long_description=open("readme.txt").read()
+	install_requires=[]
+)
